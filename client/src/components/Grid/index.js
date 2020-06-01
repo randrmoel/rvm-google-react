0035349b8bbe7cbb0fd41fc59c10ddc7d10e4187
Robert Moel <58125997+randrmoel@users.noreply.github.com>
2020-06-01 18:21:30 -0400

    Add comments

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -2,20 +2,17 @@ import React from "react";
 
 // Exporting the Container, Row, and Col components from this file
 
-// This Container component allows us to use a bootstrap 
-// container without worrying about class names
+// dynamic Container component
 export function Container({ fluid, children }) {
   return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
-// This Row component lets us use a bootstrap row without 
-// having to think about class names
+// dynamic Row Component
 export function Row({ fluid, children }) {
   return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
 
-// This Col component lets us size bootstrap columns with less syntax
-// e.g. <Col size="md-12"> instead of <div className="col-md-12">
+// Dynamic Col Component
 export function Col({ size, children }) {
   return (
     <div
